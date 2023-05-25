37c7064307b105632107d73a064668649e5f9c26
StepanErin <erinstepanvit@gmail.com>
2023-05-25 20:06:45 +0700

    a1

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace LessonGit_2023
             Console.WriteLine("C0");
             Console.WriteLine("C1");
             Console.WriteLine("a0");
+            Console.WriteLine("a1");
             Console.ReadKey();
         }
     }
